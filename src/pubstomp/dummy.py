782eaef61cc395a87d5477d5cdac561ca9e8f5e8
ml-evs <me388@cam.ac.uk>
2019-01-25 19:57:17 +0000

    Tidied up DummyEngine

--- src/pubstomp/dummy.py
+++ src/pubstomp/dummy.py
@@ -1,16 +1,17 @@
 import random
+from similarity import SimilarityEngine
+from document import Document
 
 class DummyEngine(SimilarityEngine):
-  def __init__(self, documents):
-    print('Yeah, I got nothing useful for you here.')
-  
-  @staticmethod
-  def parse_document(document):
-    print('Ha. Ha. Ha. You really think I\'m gonna parse that for you?')
-  
-  def get_similarity(self, document_a, document_b):
-    print('Okay, I\'ll look at \'em.')
-    _ = document_a.parsed(self)
-    _ = document_b.parsed(self)
-    print('Nope. Pile of stink. Have this garbage instead.')
-    return random.random()
+    def __init__(self, documents):
+        for doc in documents:
+            assert isinstance(doc, Document)
+        print('Checked document list, all documents')
+
+    @staticmethod
+    def parse_document(document):
+        return 'totally unique string'
+
+    def get_similarity(self, document_a, document_b):
+        print(document_a.parsed(self), document_b.parsed(self))
+        return random.random()
